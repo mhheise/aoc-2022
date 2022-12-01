acefa56867a8a747c92d82764ba24ce3bc10f61f
Matt Heise <mhheise@pm.me>
2022-12-01 16:45:48 -0600

    feat: day01

---
+++ src/bin/01.rs
@@ -0,0 +1,73 @@
+use anyhow::Result;
+use itertools::Itertools;
+
+fn part1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|cal| cal.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .max()
+        .unwrap()
+}
+
+fn part2(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|cal| cal.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .sorted()
+        .rev()
+        .take(3)
+        .sum::<usize>()
+}
+
+fn main() -> Result<()> {
+    let input = include_str!("../../inputs/01.txt");
+
+    let p1 = part1(&input);
+    let p2 = part2(&input);
+
+    println!("day01 -> part1: {p1} part2: {p2}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+    use pretty_assertions::assert_eq;
+
+    const SAMPLE: &str = indoc! {r#"
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    "#};
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&SAMPLE), 24000);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&SAMPLE), 45000);
+    }
+}
